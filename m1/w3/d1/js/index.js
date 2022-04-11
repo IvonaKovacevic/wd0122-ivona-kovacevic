2c7b6bab06fa7c43e8e65cb713b2bf82f5039186
Ivona Kovacevic <ivonakovacevic@Air-di-Ivona.homenet.telecomitalia.it>
2022-04-11 15:08:43 +0200

    javascript1

---
+++ m1/w3/d1/js/index.js
@@ -0,0 +1,10 @@
+
+window.alert('Sto studiando JS');
+alert('Ciao!');
+
+document.getElementById("p1").style.color = "yellow"
+document.getElementById("blocco1").style.backgroundColor = "Black"
+
+var x = 5 ;
+var y = 10 ;
+var z = x + y;
